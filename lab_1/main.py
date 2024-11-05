6d7f390142e7bb9afe06c8effa488dcd72a98c02
ynnmuraii <tihookeanski.y.tanruuu@gmail.com>
2024-11-05 23:14:46 +0400

    added type hints

--- lab_1/main.py
+++ lab_1/main.py
@@ -1,6 +1,6 @@
 from tkinter import Tk, Menu, LabelFrame, filedialog, messagebox, colorchooser, font, Text, Scrollbar, StringVar, END, INSERT, WORD
 from _tkinter import TclError
-from typing import Optional, Tuple
+from typing import Optional
 import model
 import view
 
@@ -37,7 +37,8 @@ class Window:
                        height=WINDOW_HEIGHT / 6 + 10, width=WINDOW_WIDTH - 340)
 
         # Блок форматирования
-        x, y = 10, 38
+        x = 10
+        y = 38
         view.create_label(master=settings_frame, text="Шрифт:", x=x, y=y)
         view.create_button(master=settings_frame, text="B", 
                            command=lambda: self.stylize_text(style=font.BOLD),
@@ -50,7 +51,8 @@ class Window:
                            x=x + 60, y=y + 22, height=25, width=25)
         
         # Блок выравнивания
-        x, y = 120, 38
+        x = 120
+        y = 38
         view.create_label(master=settings_frame, text="Выравнивание:", x=x, y=y)
         view.create_button(master=settings_frame, text="L", 
                            command=lambda: self.change_alignment("left"),
@@ -63,7 +65,8 @@ class Window:
                            x=x + 60, y=y + 22, height=25, width=25)
 
         # Блок undo/redo
-        x, y = 230, 38
+        x = 230
+        y = 38
         view.create_label(master=settings_frame, text="Отмена:", x=x, y=y)
         view.create_button(master=settings_frame, text="↺", 
                            command=lambda: self.undo_action(),
@@ -73,7 +76,8 @@ class Window:
                            x=x + 30, y=y + 22, height=25, width=25)
 
         # Блок страниц
-        x, y = 330, 65
+        x = 330
+        y = 65
         view.create_label(master=settings_frame, text="Страница", x=x, y=y)
         self.page_num = view.get_spinbox(x=x + 80, y=y, width=30,
                                          master=settings_frame, from_=0, to=0,
@@ -93,13 +97,15 @@ class Window:
                            x=210, y=10, height=20, width=80)
 
         # Блок цветов
-        x, y = 330, 10
+        x = 330
+        y = 10
         view.create_button(master=settings_frame, text="Цвет текста", 
                            command=lambda: self.choose_text_color(),
                            x=x, y=y, height=20, width=80)
         self.text_color = view.get_frame(master=settings_frame, bg="#000000", 
                                          x=x + 90, y=y, width=20, height=20)
-        x, y = 330, 35
+        x = 330
+        y = 35
         view.create_button(master=settings_frame, text="Цвет фона", 
                            command=lambda: self.choose_background_color(),
                            x=x, y=y, height=20, width=80)
@@ -107,7 +113,7 @@ class Window:
                                                x=x + 90, y=y, width=20, height=20)
 
         # Окно статистики
-        info_frame = LabelFrame(master=self.window, text="Параметры текста")
+        info_frame: LabelFrame = LabelFrame(master=self.window, text="Параметры текста")
         view.place_obj(info_frame, x=WINDOW_WIDTH - 320, y=10, 
                        height=WINDOW_HEIGHT / 6 + 10, width=310)
         view.create_button(master=info_frame, text="Обновить", 
@@ -159,7 +165,149 @@ class Window:
         """Открытие файла и загрузка его содержимого в текстовое поле."""
         file_path = filedialog.askopenfilename()
         try:
-            self.pages = model
+            self.pages = model.check_and_open_file(file_path)
+            self.page_num.config(from_=1, to=len(self.pages))
+            page_num = int(self.page_num.get()) - 1
+            self.text.delete(1.0, END)
+            self.curr_file = file_path
+            self.update_window_title(file_path.split("/")[-1])
+            self.text.insert(INSERT, self.pages[page_num])
+            self.update_statistics()
+        except FileNotFoundError as e:
+            if str(e) == "":
+                return
+            elif str(e) == "Неверный формат файла":
+                messagebox.showerror("Неверный формат файла", "Откройте файл с расширением .txt")
+            else:
+                print("Что-то пошло не так")
+                return
+
+    def save_file(self) -> None:
+        """Сохранение текущего файла. Если файл новый, вызывает метод сохранения под новым именем."""
+        if self.curr_file is None:
+            self.save_file_as()
+            return
+        model.save_file(self.curr_file, self.text.get(1.0, END))
+
+    def save_file_as(self) -> None:
+        """Сохранение файла под новым именем, запрашивая путь у пользователя."""
+        file_path = filedialog.asksaveasfilename()
+        if file_path == "":
+            return
+        self.curr_file = file_path
+        self.update_window_title(file_path.split("/")[-1])
+        model.save_file(self.curr_file, self.text.get(1.0, END))
+
+    def delete_file(self) -> None:
+        """Удаление текущего файла и сброс текстового поля."""
+        if self.curr_file is None:
+            messagebox.showerror("Ошибка файла", "Файла не существует")
+            return
+        model.delete_file(self.curr_file)
+        self.curr_file = None
+        self.text.delete(1.0, END)
+        self.update_window_title()
+        self.page_num.config(from_=0, to=0)
+
+    def update_window_title(self, new_name: Optional[str] = None) -> None:
+        """Обновление заголовка окна с именем файла или базовым названием."""
+        if new_name is None:
+            self.window.title("2M NotePad")
+        else:
+            self.window.title(f"2M NotePad ― {new_name}")
+
+    def change_page(self) -> None:
+        """Изменение текущей страницы текста и обновление интерфейса."""
+        try:
+            new_page_num = int(self.page_num.get()) - 1
+        except ValueError:
+            return
+        self.text.delete(1.0, END)
+        self.change_alignment(self.global_tags["align"])
+        self.text.insert(INSERT, self.pages[new_page_num])
+        self.update_statistics()
+        self.reset_find_text()
+
+    def change_alignment(self, alignment: str) -> None:
+        """Изменение выравнивания текста в соответствии с выбранным стилем."""
+        self.text.tag_configure("align", justify=alignment)
+        self.text.insert(1.0, " ")
+        self.text.tag_add("align", "1.0", "end")
+        self.global_tags["align"] = alignment
+
+    def update_statistics(self) -> None:
+        """Обновление статистики текста: количество страниц, строк и символов."""
+        page_count = max(len(self.pages), 1)
+        lines_count = self.text.count("0.0", "end", "displaylines")[0]
+        letters_count = len(self.text.get(1.0, END))
+        self.update_entry(self.pages_counter, page_count)
+        self.update_entry(self.lines_counter, lines_count)
+        self.update_entry(self.letters_counter, letters_count)
+
+    @staticmethod
+    def update_entry(entry, count: int, block: bool = True) -> None:
+        """Обновление поля ввода с заданным значением и опциональной блокировкой."""
+        entry.config(state="normal")
+        entry.delete(0, END)
+        entry.insert(0, count)
+        if block:
+            entry.config(state="readonly")
+
+    def find_text(self) -> None:
+        """Поиск и выделение всех вхождений текста, введенного пользователем."""
+        self.clear_highlighted_text()
+        prev_pos = "1.0"
+        count = StringVar()
+        str_find = self.finder.get()
+        if str_find == "":
+            return
+        counter = 0
+        self.global_tags["finder"] += 1
+        finder_tag_version = self.global_tags["finder"]
+        while True:
+            new_pos = self.text.search(str_find, prev_pos, stopindex="end", count=count)
+            if new_pos == "":
+                break
+            self.text.tag_configure(f"search{finder_tag_version}", background="#00FA9A")
+            self.text.tag_add(f"search{finder_tag_version}", new_pos, f"{new_pos} + {count.get()}c")
+            prev_pos = model.shift_pos(new_pos, count.get())
+            counter += 1
+        self.update_entry(self.word_counter, counter)
+
+    def reset_find_text(self) -> None:
+        """Сброс выделения найденного текста и очистка поля ввода поиска."""
+        self.update_entry(self.word_counter, "")
+        self.update_entry(self.finder, "", block=False)
+        self.clear_highlighted_text()
+        self.global_tags['finder'] = 0
+
+    def clear_highlighted_text(self) -> None:
+        """Очистка подсветки выделенного текста."""
+        self.text.tag_configure(f"search{self.global_tags['finder']}", background="#ffffff")
+
+    def stylize_text(self, underline: bool = False, style: str = NORMAL, font_type: str = "", font_size: str = "") -> None:
+        """Применение стиля к выделенному тексту."""
+        if font_type == "":
+            font_type = self.font_type.get()
+        if font_size == "":
+            font_size = self.font_size.get()
+        tag_name = self.global_tags["format"] + 1
+        self.text.tag_config(tag_name, font=(font_type, font_size, style), underline=underline)
+        self.text.tag_add(tag_name, "sel.first", "sel.last")
+        self.global_tags["format"] += 1
+
+    def get_font_style(self, style: str = NORMAL, font_type: str = "", font_size: str = "") -> tuple[str, str, str]:
+        """Получение текущего стиля шрифта для текста."""
+        if font_type == "":
+            font_type = self.font_type.get()
+        if font_size == "":
+            font_size = self.font_size.get()
+        return font_type, font_size, style
+
+    def undo_action(self) -> None:
+        """Отмена последнего действия пользователя."""
+        try:
+            self.text.edit_undo()
         except TclError:
             pass
 
